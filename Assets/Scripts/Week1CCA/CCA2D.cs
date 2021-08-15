e65599aa00b913b3e84111e4b3ef5d0505821cea
Alexey Shutov <kalandrill@gmail.com>
2021-08-15 20:51:12 +0300

    CCA week 1 follow along complete

---
+++ Assets/Scripts/Week1CCA/CCA2D.cs
@@ -0,0 +1,260 @@
+﻿using UnityEngine;
+using EasyButtons;
+
+public class CCA2D : MonoBehaviour
+{
+
+    const int MAX_RANGE = 10;
+    [Header("CCA Primary Params")]
+    [Range(1, MAX_RANGE)]
+    public int range = 1;
+
+    const int MAX_THRESHOLD = 25;
+    [Range(0, MAX_THRESHOLD)]
+    public int threshold = 3;
+
+    const int MAX_STATES = 20;
+    [Range(0, MAX_STATES)]
+    public int nstates = 3;
+
+    public bool moore;
+
+    [Header("CCA Secondary Params")]
+    [Range(1, MAX_RANGE)]
+    public int range2 = 1;
+
+    [Range(0, MAX_THRESHOLD)]
+    public int threshold2 = 3;
+
+    [Range(0, MAX_STATES)]
+    public int nstates2 = 3;
+
+    public bool moore2;
+
+    [Header("Setup")]
+    [Range(8, 2048)]
+    public int rez = 8;
+
+    [Range(0, 50)]
+    public int stepsPerFrame = 0;
+
+    [Range(1, 50)]
+    public int stepMod = 1;
+
+    public Material outMat;
+    public ComputeShader cs;
+
+    private RenderTexture readTex;
+    private RenderTexture writeTex;
+    private RenderTexture outTex;
+
+    private int stepKernel;
+
+    void Update()
+    {
+        if (Time.frameCount % stepMod == 0)
+        {
+            for (int i = 0; i < stepsPerFrame; i++)
+            {
+                Step();
+            }
+        }
+
+    }
+
+    /*
+     * 
+     * 
+     *  RESET 
+     * 
+     * 
+     */
+    void Start()
+    {
+        Reset();
+    }
+
+    //https://github.com/madsbangh/EasyButtons
+    [Button]
+    private void Reset()
+    {
+        readTex = CreateTexture(RenderTextureFormat.RFloat);
+        writeTex = CreateTexture(RenderTextureFormat.RFloat);
+        outTex = CreateTexture(RenderTextureFormat.ARGBFloat);
+
+        stepKernel = cs.FindKernel("StepKernel");
+
+        GPUResetKernel();
+    }
+
+
+    protected RenderTexture CreateTexture(RenderTextureFormat format)
+    {
+        RenderTexture texture = new RenderTexture(rez, rez, 1, format);
+        texture.enableRandomWrite = true;
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Repeat;
+        texture.useMipMap = false;
+        texture.Create();
+        return texture;
+    }
+
+    private void GPUResetKernel()
+    {
+        int k = cs.FindKernel("ResetKernel");
+        cs.SetTexture(k, "writeTex", writeTex);
+
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+
+        cs.SetInt("rez", rez);
+        cs.Dispatch(k, rez, rez, 1);
+        SwapTex();
+    }
+
+    [Button]
+    public void RandomizeParams()
+    {
+        var rand = new System.Random();
+        range = (int)(rand.NextDouble() * (MAX_RANGE - 1)) + 1;
+        threshold = (int)(rand.NextDouble() * MAX_THRESHOLD - 1) + 1;
+        nstates = (int)(rand.NextDouble() * (MAX_STATES - 2)) + 2;
+        moore = rand.NextDouble() <= 0.5;
+
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    [Button]
+    public void ResetAndRandomize()
+    {
+        RandomizeParams();
+        Reset();
+    }
+
+    [Button]
+    public void RandomizeColors()
+    {
+        var rand = new System.Random(Time.frameCount);
+
+        // Step 1: Create a gradient
+        Gradient g = new Gradient();
+        int keycount = 8;
+        GradientColorKey[] c = new GradientColorKey[keycount];
+        GradientAlphaKey[] a = new GradientAlphaKey[keycount];
+        // 2/1/19/n
+        float hueMax = 1, hueMin = .9f, sMax = 2, sMin = 0, vMax = 1, vMin = 0;
+        for (int i = 0; i < keycount; i++)
+        {
+            float h = (float)rand.NextDouble() * (hueMax - hueMin) + hueMin;
+            float s = (float)rand.NextDouble() * (sMax - sMin) + sMin;
+            float v = (float)rand.NextDouble() * (vMax - vMin) + vMin;
+            Color nc = Color.HSVToRGB(h, s, v);
+            c[i].color = nc;
+            a[i].time = c[i].time = (i * (1.0f / keycount));
+            a[i].alpha = 1.0f;
+        }
+        g.SetKeys(c, a);
+
+
+        // Step 2: Sample colors from the gradient
+        // this gives us more "related" colors than just selecting random colors
+        Vector4[] colors = new Vector4[nstates];
+        for (int i = 0; i < nstates; i++)
+        {
+            float t = (float)rand.NextDouble();
+            colors[i] = g.Evaluate(t);
+        }
+        cs.SetVectorArray("colors", colors);
+    }
+
+
+    public void RandomizeSecondaryParams()
+    {
+        var rand = new System.Random();
+        range2 = (int)(rand.NextDouble() * (MAX_RANGE - 1)) + 1;
+        threshold2 = (int)(rand.NextDouble() * MAX_THRESHOLD - 1) + 1;
+        nstates2 = (int)(rand.NextDouble() * (nstates - 2)) + 2;
+        moore2 = rand.NextDouble() <= 0.5;
+
+        cs.SetInt("range", range2);
+        cs.SetInt("threshold", threshold2);
+        cs.SetInt("nstates", nstates2);
+        cs.SetBool("moore", moore2);
+    }
+
+    [Button]
+    public void RandomizeSecondaryParamsWithNoise()
+    {
+        RandomizeSecondaryParams();
+        AddNoise();
+    }
+
+
+
+    [Button]
+    public void AddNoise()
+    {
+        int kernel = cs.FindKernel("SecondaryNoiseKernel");
+        cs.SetTexture(kernel, "readTex", readTex);
+        cs.SetTexture(kernel, "writeTex", writeTex);
+        cs.Dispatch(kernel, rez, rez, 1);
+
+        SwapTex();
+    }
+
+    [Button]
+    public void SetPrimaryParams()
+    {
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    [Button]
+    public void SetSecondaryParams()
+    {
+        cs.SetInt("range", range2);
+        cs.SetInt("threshold", threshold2);
+        cs.SetInt("nstates", nstates2);
+        cs.SetBool("moore", moore2);
+    }
+
+
+    /*
+     * 
+     * 
+     *  STEP 
+     * 
+     * 
+     */
+    [Button]
+    public void Step()
+    {
+        cs.SetTexture(stepKernel, "readTex", readTex);
+        cs.SetTexture(stepKernel, "writeTex", writeTex);
+        cs.SetTexture(stepKernel, "outTex", outTex);
+
+        cs.Dispatch(stepKernel, rez, rez, 1);
+
+        SwapTex();
+
+        outMat.SetTexture("_UnlitColorMap", outTex);
+    }
+
+
+    private void SwapTex()
+    {
+        RenderTexture tmp = readTex;
+        readTex = writeTex;
+        writeTex = tmp;
+    }
+
+
+
+}
--- Assets/Scripts/Week1CCA/CCAManager.cs
+++ Assets/Scripts/Week1CCA/CCAManager.cs
@@ -1,11 +1,276 @@
+using System.IO;
+using EasyButtons;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CCAManager : MonoBehaviour
 {
+    private const int MaxNumStates = 32;
+    private const int MaxThreshold = 25;
+    private const int MaxRange = 20;
+    private const string s_ReadTexID = "_ReadTexture";
+    private const string s_WriteTexID = "_WriteTexture";
+
+    [SerializeField] 
+    private ComputeShader caaCompute;
+    [SerializeField] 
+    private int resolution;
+    [SerializeField, Range(1, MaxThreshold)] 
+    private int threshold;
+    [SerializeField, Range(0,1)] 
+    private float sampleThreshold = 0.5f;
+    [SerializeField, Range(2, MaxNumStates)] 
+    private int numStates;
+    [SerializeField, Range(1, MaxRange)] 
+    private int range;
+    [SerializeField] 
+    private bool moore;
+    [SerializeField, Range(1, 50)] 
+    private int stepsPerFrame;
+    [SerializeField, Range(0, 50)] 
+    private int framesToSkip;
+    [SerializeField] 
+    private Material caaMaterial;
+    [SerializeField] 
+    private Material maskMaterial;
+    [SerializeField] 
+    private ColorMode colorMode;
+
+    [SerializeField] 
+    private int maskRez;
+    
+    [SerializeField] 
+    private int maskRezTrue;
 
 
-    void Update()
+    private RenderTexture _maskReadTexture;
+    private RenderTexture _maskWriteTexture;
+    private RenderTexture _maskOutTexture;
+    private RenderTexture _readTexture;
+    private RenderTexture _writeTexture;
+    private RenderTexture _outTexture;
+    private int _resetKernelId;
+    private int _simulateKernelId;
+    private static readonly int s_MainTex = Shader.PropertyToID("_UnlitColorMap");
+    private int _dispatchThreadCount;
+    private int _maskThreadCount;
+    private bool _isSimStarted;
+
+    public void Start()
     {
-        
+        SetKernels();
+        RandomizeColors();
+        StartSimulation();
     }
+    
+    public void Update()
+    {
+        if (Time.frameCount % (framesToSkip + 1) != 0) return;
+
+        for (int i = 0; i < stepsPerFrame; i++)
+        {
+            caaCompute.SetTexture(_simulateKernelId, "_Mask", _maskOutTexture);
+            caaCompute.SetTexture(_simulateKernelId, "_OutTexture", _outTexture);
+            caaCompute.SetTexture(_simulateKernelId, s_ReadTexID, _readTexture);
+            caaCompute.SetTexture(_simulateKernelId, s_WriteTexID, _writeTexture);
+            caaCompute.Dispatch(_simulateKernelId, _dispatchThreadCount, _dispatchThreadCount, 1);
+            SwapTextures();
+            
+            
+            caaCompute.SetTexture(_simulateKernelId, "_OutTexture", _maskOutTexture);
+            caaCompute.SetTexture(_simulateKernelId, "_Mask", _outTexture);
+            caaCompute.SetTexture(_simulateKernelId, s_ReadTexID, _maskReadTexture);
+            caaCompute.SetTexture(_simulateKernelId, s_WriteTexID, _maskWriteTexture);
+            caaCompute.Dispatch(_simulateKernelId, _dispatchThreadCount, _dispatchThreadCount, 1);
+            SwapTexturesMask();
+        }
+    }
+  
+    [Button]
+    private void StartSimulation()
+    {
+        _maskThreadCount = maskRezTrue / 16;
+        _dispatchThreadCount = resolution / 16;
+        CreateTextures();
+        CreateMaskTextures();
+        caaMaterial.SetTexture(s_MainTex, _outTexture);
+        maskMaterial.SetTexture(s_MainTex, _maskOutTexture);
+        ApplyParams();
+        ResetReadTexture();
+        ResetMaskReadTexture();
+    }
+
+    [Button]
+    private void StopSimulation() => _isSimStarted = false;
+
+    [Button]
+    private void RandomizeAndStart()
+    {
+        _dispatchThreadCount = resolution / 16;
+        _maskThreadCount = maskRezTrue / 16;
+        CreateTextures();
+        CreateMaskTextures();
+        caaMaterial.SetTexture(s_MainTex, _outTexture);
+        maskMaterial.SetTexture(s_MainTex, _maskOutTexture);
+        RandomizeParameters();
+        RandomizeColors();
+        ResetReadTexture();
+        ResetMaskReadTexture();
+    }
+
+    [Button]
+    private void RandomizeParameters()
+    {
+        numStates = Random.Range(2, MaxNumStates + 1);
+        threshold = Random.Range(1, MaxThreshold + 1);
+        range = Random.Range(1, MaxRange);
+        moore = Random.Range(0, 1f) > 0.5;
+        ApplyParams();
+    }
+
+    [Button]
+    private void RandomizeColorMode()
+    {
+        colorMode = (ColorMode)Random.Range(0, (int)ColorMode.StateFull + 1);
+        caaCompute.SetInt("_ColorMode", (int)colorMode);
+    }
+    
+    [Button]
+    private void RandomizeColors()
+    {
+        var grad = new Gradient();
+        var keys = 8;
+
+        var colorKeys = new GradientColorKey[keys];
+        var alphaKeys = new GradientAlphaKey[keys];
+        
+        for (int i = 0; i < keys; i++)
+        {
+            var t = (float)i / (keys - 1);
+            
+            var randColor = Random.ColorHSV(0f, 1, 0, 1, 0, 1);
+            
+            colorKeys[i] = new GradientColorKey(randColor, t);
+            alphaKeys[i] = new GradientAlphaKey(1, t);
+        }
+        
+        grad.SetKeys(colorKeys, alphaKeys);
+
+        var colorArray = new Vector4[numStates];
+        
+        for (int i = 0; i < numStates; i++) 
+            colorArray[i] = grad.Evaluate(Random.value);
+        
+        caaCompute.SetVectorArray("_StateColors", colorArray);
+    }
+
+    [Button]
+    private void RandomizeParametersMildly()
+    {
+        numStates = Random.Range(numStates - 2, numStates + 3);
+        threshold = Random.Range(threshold - 2, threshold + 3);
+        range = Random.Range(range - 2, range + 3);
+        moore = Random.Range(0, 1f) > 0.5;
+        ApplyParams();
+    }
+
+    [Button]
+    private void ApplyParams()
+    {
+        caaCompute.SetInt("_NumStates", numStates);
+        caaCompute.SetInt("_Threshold", threshold);
+        caaCompute.SetInt("_Range", range);
+        caaCompute.SetBool("_Moore", moore);
+        caaCompute.SetInt("_Rez", resolution);
+        caaCompute.SetInt("_ColorMode", (int)colorMode);
+        caaCompute.SetInt("_MaskRez", maskRez);
+        caaCompute.SetFloat("_SampleThreshold", sampleThreshold);
+    }
+
+
+    [Button]
+    private void SaveCurrentParams()
+    {
+        var textPath = Path.Combine(Application.dataPath, "Resources", "SelectedCCAs.txt");
+        var text = $"\nThreshold: {threshold}, States: {numStates}, Range: {range}, Moore: {moore}, ColorMode: {colorMode}";
+        using (var appendStream = File.AppendText(textPath))
+        {
+            appendStream.Write(text);
+            appendStream.Close();
+        }
+    }
+    
+    private void ResetReadTexture()
+    {
+        caaCompute.SetTexture(_resetKernelId, s_WriteTexID, _writeTexture);
+        caaCompute.Dispatch(_resetKernelId, _dispatchThreadCount, _dispatchThreadCount, 1);
+        SwapTextures();
+    }
+    
+    private void ResetMaskReadTexture()
+    {
+        caaCompute.SetTexture(_resetKernelId, s_WriteTexID, _maskWriteTexture);
+        caaCompute.Dispatch(_resetKernelId, _dispatchThreadCount, _dispatchThreadCount, 1);
+        SwapTexturesMask();
+    }
+    
+    private void CreateTextures()
+    {
+        _outTexture = CreateRenderTex(RenderTextureFormat.ARGB32, resolution);
+        _writeTexture = CreateRenderTex(RenderTextureFormat.RFloat, resolution);
+        _readTexture = CreateRenderTex(RenderTextureFormat.RFloat, resolution);
+    }
+
+    private void CreateMaskTextures()
+    {
+        _maskOutTexture = CreateRenderTex(RenderTextureFormat.ARGB32, maskRezTrue);
+        _maskWriteTexture = CreateRenderTex(RenderTextureFormat.RFloat, maskRezTrue);
+        _maskReadTexture = CreateRenderTex(RenderTextureFormat.RFloat, maskRezTrue);
+    }
+    
+    private RenderTexture CreateRenderTex(RenderTextureFormat format, int res)
+    {
+        var renderTex = new RenderTexture(res, res, 1, format);
+        renderTex.useMipMap = false;
+        renderTex.filterMode = FilterMode.Point;
+        renderTex.enableRandomWrite = true;
+        renderTex.wrapMode = TextureWrapMode.Repeat;
+        renderTex.Create();
+        
+        return renderTex;
+    }
+
+    private void SwapTextures()
+    {
+        var tmp = _readTexture;
+        _readTexture = _writeTexture;
+        _writeTexture = tmp;
+    }
+    
+    private void SwapTexturesMask()
+    {
+        var tmp = _maskReadTexture;
+        _maskReadTexture = _maskWriteTexture;
+        _maskWriteTexture = tmp;
+    }
+    
+    private void SetKernels()
+    {
+        _resetKernelId = caaCompute.FindKernel("Reset");
+        _simulateKernelId = caaCompute.FindKernel("Simulate");
+    }
+}
+
+public enum ColorMode
+{
+    StateNormalized,
+    StateFade,
+    ThresholdNormalized,
+    ThresholdFade,
+    BasicColor,
+    ColorRange,
+    ColorRangeFade,
+    ThresholdRangeColor,
+    ThresholdRangeColorFade,
+    StateFull
 }
